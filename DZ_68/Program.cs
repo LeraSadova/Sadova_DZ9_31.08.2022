4530e6a91dd97d241ae7b8d3b3ec3821c0000644
Садова Валерия Андреевна <SadovaVA@vseinstrumenti.ru>
2022-08-31 11:06:16 +0300

    68

--- DZ_68/Program.cs
+++ DZ_68/Program.cs
@@ -2,31 +2,29 @@
 //Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 29
 
-// A(n,m) = m+1, если n = 0
-// = A(n-1,1), если n!=0, m = 0
-// = A(n-1,A(n,m-1)), если n>0, m > 0
-
 Console.Clear();
-Console.Write("Введи первое целое число n: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введи второе целое число m: ");
+Console.Write("Введи первое целое число m: ");
 int m = Convert.ToInt32(Console.ReadLine());
-
-
-int functionAkkerman = Ack(m, n);
+Console.Write("Введи второе целое число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+  
+//сообщение о том, что числа должны быть неотрицательными
+if (m<0) Console.WriteLine("числа должны быть положительные! начните заново!!! ");
+  else if (n<0) Console.WriteLine("числа должны быть положительные! начните заново!!! ");
+else 
+{
+//решение для функции Акермана
+int functionAkkerman = Akkerman(m, n);
 
 Console.Write($"Функция Аккермана = {functionAkkerman} ");
 
-int Ack(int m, int n)
-{
-  if (m == 0) return n + 1;
-  else if (n == 0) return Ack(m - 1, 1);
-  else return Ack(m - 1, Ack(m, n - 1));
-}
-
-int InputNumbers(string input) 
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
+  int Akkerman(int m, int n)
+  {
+    if (m == 0) return n + 1; // A(m,n) = n+1, если m = 0
+    else if ((m > 0) && (n == 0)) return Akkerman(m - 1, 1); // = A(m-1,1), если m!=0, n = 0
+    else if ((m > 0) && (n > 0)) 
+    // = A(m-1,A(m,n-1)), если m>0, n > 0
+            return Akkerman(m - 1, Akkerman(m, n - 1)); 
+    else return 0;
+   }
 }
